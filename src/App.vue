366d764616b278dce011ee6ffd121aa1a61b826e
bellice <bertrand.miroux@cget.gouv.fr>
2020-01-13 18:41:45 +0100

    add vue cli

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="l-container">
+      <SectionMap />
+      <NavMap />
+    </div>
+  </div>
+</template>
+
+<script>
+import SectionMap from "./components/SectionMap.vue";
+import NavMap from "./components/NavMap.vue";
+
+export default {
+  name: "app",
+  components: {
+    SectionMap,
+    NavMap
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./styles/global.scss";
+</style>
---
+++ src/components/SectionMap.vue
@@ -0,0 +1,3 @@
+<template>
+  <section>Section</section>
+</template>
