671feca29466b3c1ea3bc4947f27131106969ee3
bellice <bellice@tuta.io>
2020-01-18 18:03:27 +0100

    reduce architecture

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,21 @@
 
     <div class="l-container">
       <section>
-
-    <div v-if="isDisplayed">
-        <router-view ></router-view>
-    </div>
+      <div class="l-section-content">
+        <div class="l-content">
+            <router-view ></router-view>
+        </div>
+      </div>
       </section>
 
       <nav>
-        <div id="l-nav-button">
-          <div class="nav-home nav-item">
-          <router-link class="nav-button" :to="{ name: 'a' }" tag="button"
-            ><span>Accueil</span></router-link
-            >
-          </div>
+        <NavigationButton/>
 
-          <div class="nav-map nav-item" ref="navmap">
-            <router-link class="nav-button" :to="{ name: 'b' }" tag="button"
-              ><span>Carte</span></router-link
-            >
-          </div>
-
-          <div class="nav-legend nav-item">
-            <router-link class="nav-button" :to="{ name: 'c' }" tag="button"
-              ><span>Légende</span></router-link
-            >
-          </div>
-        </div>
         <div id="l-nav-content">
-          <div>
-        <router-view ></router-view>
-    </div>
+
+          <div class="l-content">
+           <router-view ></router-view>
+         </div>
 
         </div>
       </nav>
@@ -43,44 +28,12 @@
 
 <script>
 
+import NavigationButton from "./components/NavigationButton.vue"
 
 export default {
   name: "app",
   components: {
-
-  },
-  data (){
-    return{
-      isDisplayed: true
-    }
-  },
-  created(){
-    window.addEventListener("resize", this.myEvent)
-  },
-  destroyed(){
-    window.addEventListener("resize", this.myEvent)
-  },
-  methods:{
-    myEvent(){
-      if(this.$refs.navmap.clientWidth == 0){
-        // eslint-disable-next-line no-console
-        this.isDisplayed = false
-      } else{
-        // eslint-disable-next-line no-console
-        this.isDisplayed = true
-      }
-    }
-  },
-  mounted: function(){
-    this.$watch(
-      function (){
-        // eslint-disable-next-line no-console
-        console.log(this.$refs.navmap.clientWidth)
-        // eslint-disable-next-line no-console
-       console.log(this.isDisplayed)
-      }
-    )
-   
+    NavigationButton
   }
 };
 
---
+++ src/components/NavigationButton.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="l-nav-button">
+          <div class="nav-home nav-item">
+          <router-link class="nav-button" :to="{ name: 'a' }" tag="button"
+            ><span>Accueil</span></router-link
+            >
+          </div>
+
+          <div class="nav-map nav-item" ref="navmap">
+            <router-link class="nav-button" :to="{ name: 'b' }" tag="button"
+              ><span>Carte</span></router-link
+            >
+          </div>
+
+          <div class="nav-legend nav-item">
+            <router-link class="nav-button" :to="{ name: 'c' }" tag="button"
+              ><span>Légende</span></router-link
+            >
+          </div>
+          </div>
+</template>
+
+
+<script>
+export default {
+  name: "NavigationButton",
+  mounted: function(){
+    this.$watch(
+      function (){
+        // eslint-disable-next-line no-unsed-vars
+        //observer.observe(this.$refs.navmap, { attributes: true});
+        // eslint-disable-next-line no-console
+        console.log(this.$refs.navmap)
+      }
+    ) 
+  }
+};
+
+</script>
